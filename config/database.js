9b4513fbef4ca9fad6f1ea42169409360cdb244b
jose <jose.bossa.3@gmail.com>
2026-06-01 22:03:09 -0300

    volvio como antes por que se rompio todo

--- config/database.js
+++ config/database.js
@@ -1,13 +1,20 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
-  }
-});
+);
 
 module.exports = sequelize;
